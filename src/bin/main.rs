be8e903be0b3075531972ae056bad8e5e900e6ce
s4i <yasasisanoriyuu123@yahoo.co.jp>
2019-03-03 21:01:52 +0900

    Goodbye args.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate lazy_static;
+extern crate structopt;
 
 use regex::Regex;
 use select::document::Document;
@@ -14,8 +15,6 @@ use std::result;
 use std::sync::Mutex;
 use std::{fs, str};
 
-pub mod args;
-
 lazy_static! {
     static ref BUILD_OK: Mutex<Vec<String>> = {
         let v: Vec<String> = Vec::new();
@@ -24,6 +23,37 @@ lazy_static! {
 }
 
 const FILE_PATH: &str = "latest.txt";
+
+mod args {
+    use structopt::StructOpt;
+    #[derive(Debug, StructOpt)]
+    pub enum Cargo {
+        #[structopt(
+            name = "rls-install",
+            about = "Install the Rust and Rust Language Server",
+            author = ""
+        )]
+        Install(Channel),
+    }
+
+    #[derive(Debug, StructOpt)]
+    pub struct Channel {
+        #[structopt(short, long, help = "Pre-approval Rust and RLS install command")]
+        pub yes: bool,
+        #[structopt(short, long, help = "Install stable channel Rust and RLS")]
+        pub stable: bool,
+        #[structopt(short, long, help = "Install beta channel Rust and RLS")]
+        pub beta: bool,
+        #[structopt(short, long, help = "Install nightly channel Rust and RLS")]
+        pub nightly: bool,
+    }
+
+    pub fn parse() -> Channel {
+        match StructOpt::from_args() {
+            Cargo::Install(opt) => opt,
+        }
+    }
+}
 
 fn main() {
     // Command line
